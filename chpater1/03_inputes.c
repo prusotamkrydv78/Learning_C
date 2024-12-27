1739147e37451e91f504e9192b3789ac970fe5f1
prusotamkrydv78 <prusotamkumaryadav78@gmail.com>
2024-12-27 11:28:25 +0545

    nodemon setuping for c as a live watcher

--- chpater1/03_inputes.c
+++ chpater1/03_inputes.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int a;
     char b; // b is declared but not used, so you might want to use or remove it.
 
-    printf("Enter an integer: ");
-    scanf("%d", &a);
+    // // printf("Enter an integer: ");
+    // // scanf("%d", &a);
 
+    // printf("Enter an character: ");
+    // scanf("%c", &b);
 
-    printf("Enter an character: ");
-    scanf("%c", &b);
-
-    // Correctly print the value of 'a'
-    printf("You entered integer: %d\n", a);
-    printf("You entered character: %c\n", b);
+    // // Correctly print the value of '2a'
+    // // printf("You entered integer: %d\n", a);
+    // printf("You entered character: %c\n", b);
+    printf("hello world in c\n");
 
     return 0;
 }
