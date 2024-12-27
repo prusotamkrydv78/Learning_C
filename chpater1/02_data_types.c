2b89380ee4869fe0a72e968282998ee8184827ef
prusotamkrydv78 <prusotamkumaryadav78@gmail.com>
2024-12-27 10:26:33 +0545

    learing data types

---
+++ chpater1/02_data_types.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a = 9;//'int' is used to store integer datatypes
+    float b = 23.4; //'float' is used to store decimal datatypes
+    char c = 'character'; // 'char' is used to store string datatypes
+}
