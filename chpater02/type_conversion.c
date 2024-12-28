c8235cfd143f3e5a4bec973fa933c30911838aea
prusotamkrydv78 <prusotamkumaryadav78@gmail.com>
2024-12-28 11:41:18 +0545

    chapter 2 completed

---
+++ chpater02/type_conversion.c
@@ -0,0 +1,8 @@
+#include<stdio.h> 
+int main(){  
+    int a = 9;
+    int b = 2;
+    int result = a/b;
+    printf("the result is %d",result);
+     return 0;
+} 
---
+++ chpater02/variable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a = 6;
+    int b = 3;
+    int c = 5;
+    printf("this is the value of a = %d and here is the value of b = %c", a, b ); // %d,%f and %c are called formate spcificer
+    return 0;
+}
